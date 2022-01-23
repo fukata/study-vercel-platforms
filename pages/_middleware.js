8a17e63aec5cb0b307714eaf7642296505b3acbf
fukata <tatsuya.fukata@gmail.com>
2022-01-23 15:57:34 +0900

    Googleログインできるところまで実装
    - id_tokenの値が191文字より長くなるのでtext型に変更
    - ローカルでもGoogle OAuthができるようにドメイン回りの処理を修正

--- pages/_middleware.js
+++ pages/_middleware.js
@@ -3,6 +3,7 @@ import { NextResponse } from "next/server";
 export default function middleware(req) {
   const { pathname } = req.nextUrl; // get pathname of request (e.g. /blog-slug)
   const hostname = req.headers.get("host"); // get hostname of request (e.g. demo.vercel.pub)
+  const baseAppHostname = process.env.BASE_APP_HOSTNAME;
 
   if (hostname === "vercel.pub") {
     // only for demo purposes – remove this if you want to use your root domain as the landing page
@@ -12,14 +13,16 @@ export default function middleware(req) {
   const currentHost =
     process.env.NODE_ENV === "production" && process.env.VERCEL === "1"
       ? hostname.replace(`.vercel.pub`, "")
-      : hostname.replace(`.localhost:3000`, "");
+      : hostname.replace(`.${baseAppHostname}`, "");
+
+  console.log("currentHost=%o, pathname", currentHost, pathname);
 
   if (pathname.startsWith(`/_sites`)) {
     return new Response(null, { status: 404 });
   }
 
   if (!pathname.includes(".") && !pathname.startsWith("/api")) {
-    if (currentHost == "app") {
+    if (currentHost == "app" || currentHost === baseAppHostname) {
       if (
         pathname === "/login" &&
         (req.cookies["next-auth.session-token"] ||
@@ -28,7 +31,7 @@ export default function middleware(req) {
         return NextResponse.redirect("/");
       }
       return NextResponse.rewrite(`/app${pathname}`);
-    } else if (hostname === "localhost:3000") {
+    } else if (hostname === baseAppHostname) {
       return NextResponse.rewrite(`/home`);
     } else {
       return NextResponse.rewrite(`/_sites/${currentHost}${pathname}`);
